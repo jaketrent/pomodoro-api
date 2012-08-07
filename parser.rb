e10e3a28261b591a58833a3aa2eaffd908e1fd69
Han <hkfortytwo@gmail.com>
2012-08-06 20:47:42 -0700

    slight refactoring

--- parser.rb
+++ parser.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'nokogiri'
 require 'active_support/core_ext/numeric/time.rb'
 class Parser
+  TYPES = ["Pomodoro", "Short Break", "Long Break"]
   attr_accessor :username, :pomodoro_url
   def initialize(username)
     self.username = username
@@ -21,16 +22,8 @@ class Parser
   end
   def pomodoros
     doc = nokogirize(self.content)
-    pomos = []
-    doc.css('li').each do |li|
-      pomo={}
-      type = "Pomodoro" if li.children.text["Pomodoro"]
-      type = 'Short Break' if li.children.text["Short Break"]
-      type = "Long Break" if li.children.text["Long Break"]
-      time_text = li.children.css(".time").text
-      pomo["type"] = type
-      pomo["time"] = parse_time(time_text)
-      pomos.push(pomo)
+    pomos = doc.css('li').map do |li|
+      hasherize_pomodoros(li)
     end
     return pomos
   end
@@ -38,10 +31,19 @@ class Parser
     if time_text["about"]
       string_time = time_text.split("about\s").last.split("\n").first
       string_time = string_time.split("\s").map {|s| if s["\d"] then s.to_i else s end }
-      approximate_time = Time.now - string_time[0].to_i.send(string_time[1])
+      approximate_time = string_time[0].to_i.send(string_time[1]).ago
     else 
-      approximate_time = Time.now - 1.minute
+      approximate_time = 1.minute.ago
     end
     return approximate_time
+  end
+  private
+  def hasherize_pomodoros(list_item, pomo_type = "")
+    TYPES.each {|t| pomo_type = t if list_item.children.text[t] }
+    time_text = list_item.children.css(".time").text
+    pomo = {}
+    pomo["type"] = pomo_type
+    pomo["time"] = parse_time(time_text)
+    pomo
   end
 end
